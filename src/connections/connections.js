9dfd44ded9fc5a13d7a5d26b4271b48d7ed85a9b
Md Imran <xsmartarman123@gmail.com>
2025-12-08 18:57:59 +0530

    Your commit message

--- src/connections/connections.js
+++ src/connections/connections.js
@@ -1,16 +1,19 @@
+// connections/connections.js
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  if (mongoose.connections[0].readyState) {
-    console.log("MongoDB already connected");
-    return;
+let cached = global.mongoose;
+if (!cached) cached = global.mongoose = { conn: null, promise: null };
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+  
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URL).then((mongoose) => {
+      return mongoose;
+    });
   }
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("MongoDB connected successfully");
-  } catch (err) {
-    console.error("MongoDB connection failed:", err.message);
-  }
-};
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 export default connectDB;
