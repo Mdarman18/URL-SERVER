07d485bc385db16036d739108e05bda22b097391
Md Imran <xsmartarman123@gmail.com>
2025-12-08 18:06:25 +0530

    Update CORS origin for new frontend deployment

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 import cors from "cors";
 app.use(cors({
   origin: [
-    "https://url-frontend-theta.vercel.app", // your deployed frontend
+    "https://url-frontend-joyx.vercel.app/", // your deployed frontend
     "http://localhost:5173" // development frontend
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
