152fa1a624ee7d8587c6d0672c298c61d756c7fc
Md Imran <xsmartarman123@gmail.com>
2025-12-09 10:52:05 +0530

    Fix backend for Vercel deployment

---
+++ index.js
@@ -0,0 +1,32 @@
+// -=========import importent files-=================
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+import connectDB from "./src/connections/connections.js";
+import new2 from "./src/routes/routes.js";
+dotenv.config();
+const port = process.env.PORT || 3100;
+
+// --=============connection to db-==================
+await connectDB();
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+import cors from "cors";
+// --- CORS setup ---
+app.use(
+  cors({
+    origin: "https://url-frontend-joyx.vercel.app",
+    credentials: true,
+  })
+);
+
+// -=========routes-=====================
+app.get("/", (req, res) => {
+  res.send("success");
+});
+app.use("/api", new2);
+// app.listen(port, () => {
+//   console.log(`running ${port}`);
+// });
+export default app;
--- src/connections/connections.js
+++ src/connections/connections.js
@@ -1,19 +1,16 @@
 // connections/connections.js
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-let cached = global.mongoose;
-if (!cached) cached = global.mongoose = { conn: null, promise: null };
-
-async function connectDB() {
-  if (cached.conn) return cached.conn;
-  
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URL).then((mongoose) => {
-      return mongoose;
-    });
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ Error connecting MongoDB:", err.message);
+    process.exit(1);
   }
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+};
 
 export default connectDB;
