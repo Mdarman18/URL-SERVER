3466a35dfdd92dd1c904c2e9059442670f3609a2
Md Imran <xsmartarman123@gmail.com>
2025-12-13 11:39:39 +0530

    Fix CORS origin mismatch

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ import cors from "cors";
 app.use(
   cors({
     origin:
-      "https://url-frontend-rho.vercel.app/",
+      "https://url-frontend-rho.vercel.app",
     credentials: true,
   })
 );
